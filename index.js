b17f0e201135ee0c2b5ea4f8feba5ff5caa1509a
abumusa8 <abumusa0740@gmail.com>
2025-05-29 00:27:24 +0600

    roll base acess

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
 // MongoDB URI
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
